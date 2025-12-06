c6859259aef8d63d3dc3ec56ed3b60ed12dfd03d
archie012526 <archieboleche5@gmail.com>
2025-12-06 13:09:42 +0800

    added new htmls files and fixed the landing page no design yet

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,3 +5,18 @@ def homepage(request):
 
 def landingpage(request):
     return render(request, "main/landing.html")
+
+def jobs_page(request):
+    return render(request, "main/jobs.html")
+
+def about_page(request):
+    return render(request, "main/about.html")
+
+def login_page(request):
+    return render(request, "main/login.html")
+
+def find_job_page(request):
+    return render(request, "main/find_job.html")
+
+def contact_us_page(request):
+    return render(request, "main/contact_us.html")
