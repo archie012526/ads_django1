4dd9f06043af1f3d88cdf76e0950f5801796a817
archie012526 <archieboleche5@gmail.com>
2025-12-13 15:51:08 +0800

    added functional email

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -148,3 +148,18 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ======================
+# EMAIL CONFIG (GMAIL)
+# ======================
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
