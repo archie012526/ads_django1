05f5244e5917a4f05088d7f7501ef215c6805610
archie012526 <archieboleche5@gmail.com>
2025-12-16 00:06:40 +0800

    aawfgawgf

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -175,3 +175,12 @@ class ContactSubmission(models.Model):
 
     def __str__(self):
         return f"Message from {self.name}"
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.created_at}"
