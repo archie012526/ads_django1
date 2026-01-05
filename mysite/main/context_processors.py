7b50bea02e6669ffa314f865fd6b1a0b4ecec5e6
archie012526 <archieboleche5@gmail.com>
2026-01-05 15:53:07 +0800

    admin notif

--- mysite/main/context_processors.py
+++ mysite/main/context_processors.py
@@ -1,13 +1,17 @@
-from .models import Notification
+from .models import Notification, GlobalNotification
 
 def employer_notifications(request):
-    """Add notification data to all employer templates"""
+    """Add notification data to all employer templates (and global site announcements)."""
+    # Global site announcements (shown in admin base)
+    global_notifications = GlobalNotification.objects.filter(show_on_site=True, is_active=True).order_by('-created_at')[:5]
+
     if request.user.is_authenticated and hasattr(request.user, 'profile') and request.user.profile.role == 'employer':
         recent_notifications = Notification.objects.filter(user=request.user).order_by('-created_at')[:5]
         unread_notifications_count = Notification.objects.filter(user=request.user, is_read=False).count()
-        
+
         return {
             'recent_notifications': recent_notifications,
             'unread_notifications_count': unread_notifications_count,
+            'global_notifications': global_notifications,
         }
-    return {}
+    return { 'global_notifications': global_notifications }
